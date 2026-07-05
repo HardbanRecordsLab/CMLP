a782317cee4e66ac336dafa2a348d8b1ab5337d4
Hardban Records Lab <hardbanrecordslab.pl@gmail.com>
2026-07-05 03:27:18 +0200

    fix: correct import path for getApiUrl in Login.tsx

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { getApiUrl } from '../utils';
+import { getApiUrl } from '../../utils';
 
 interface User {
   uid: string;
